2c99b2360b00ecdff44ffc4d2e65a2403940d561
PtDr <onjamamonjy@gmail.com>
2016-06-15 19:42:38 +0300

    formulaire sampana

--- src/backBundle/Resources/public/ckeditor/config.js
+++ src/backBundle/Resources/public/ckeditor/config.js
@@ -4,6 +4,15 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+
+
+	config.filebrowserBrowseUrl = '../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=flash';
+
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
