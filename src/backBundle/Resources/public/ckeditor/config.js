b4107c436ce20e8d6fa5790e62968670a6104c8f
PtDr <onjamamonjy@gmail.com>
2016-07-07 09:51:37 +0300

    before update2

--- src/backBundle/Resources/public/ckeditor/config.js
+++ src/backBundle/Resources/public/ckeditor/config.js
@@ -6,13 +6,26 @@
 CKEDITOR.editorConfig = function( config ) {
 
         config.extraPlugins = 'oembed';
-        
-	config.filebrowserBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=files';
-	config.filebrowserImageBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=images';
-	config.filebrowserFlashBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=flash';
-	config.filebrowserUploadUrl = '../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=files';
-	config.filebrowserImageUploadUrl = '../../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=images';
-	config.filebrowserFlashUploadUrl = '../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=flash';
+    
+        /*************		DEV		**************/
+
+//	config.filebrowserBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=files';
+//	config.filebrowserImageBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=images';
+//	config.filebrowserFlashBrowseUrl = '../../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=flash';
+//	config.filebrowserUploadUrl = '../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=files';
+//	config.filebrowserImageUploadUrl = '../../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=images';
+//	config.filebrowserFlashUploadUrl = '../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=flash';
+
+
+	    /*************		PROD		**************/
+
+
+	config.filebrowserBrowseUrl = '../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '../../../bundles/back/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '../../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '../../../bundles/back/kcfinder/upload.php?opener=ckeditor&type=flash';
 
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
